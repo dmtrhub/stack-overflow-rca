ad3597805cc499d06e1972a8df880513d1110d08
dmtrhub <dmtr.develop@gmail.com>
2025-09-02 15:04:29 +0200

    Initial commit - Solution structure

---
+++ AdminToolsConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Azure.Data.Tables;
+using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+// Azure Storage
+var connectionString = configuration.GetConnectionString("AzureStorage");
+var tableClient = new TableClient(connectionString, "AlertEmails");
---
+++ README.md
@@ -0,0 +1,13 @@
+# StackOverflow RCA Projekt
+
+## Potrebni alati:
+- .NET 8 SDK
+- Visual Studio 2022
+- Azure Storage Emulator
+- SQL Server LocalDB
+
+## Pokretanje:
+1. Pokrenuti Azure Storage Emulator
+2. `dotnet restore`
+3. `dotnet build`
+4. Pokrenuti projekte redom
